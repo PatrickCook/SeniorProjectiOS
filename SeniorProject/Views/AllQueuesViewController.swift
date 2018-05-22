186ccd9dffa7f9eff6b4f3b7fc59da201ab7fb74
Patrick Cook <pcookcollege@gmail.com>
2018-05-21 17:22:45 -0700

    fixed things

--- SeniorProject/Views/AllQueuesViewController.swift
+++ SeniorProject/Views/AllQueuesViewController.swift
@@ -125,10 +125,10 @@ class AllQueuesViewController: UIViewController, UITableViewDelegate, UITableVie
         fetchQueues()
     }
     
-    /*@IBAction func openMusicPlayerTapped(_ sender: Any) {
+    @IBAction func openMusicPlayerTapped(_ sender: Any) {
         print("open music player")
         if let mvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MusicPlayerViewController") as? MusicPlayerViewController {
             self.present(mvc, animated: true, completion: nil)
         }
-    }*/
+    }
 }
